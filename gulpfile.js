a772687e0fe50c6de41a15a37242d76d4d859347
NikolayShavar <fargon45821@gmail.com>
2024-01-31 21:01:55 +0200

    1

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const {src, dest, watch, parallel} = require('gulp'); //основной плагин
+const sass = require('gulp-sass')(require('sass')); //плагин для стилей
+const concat = require('gulp-concat')               //плагин для переименований и минификаций файлок, еще он вроде объединять умеет. 
+const uglify = require('gulp-uglify-es').default;  // плагин для js 
+const browserSync = require('browser-sync').create();// живой сервер, показывает изменения в браузере 
+
+
+
+
+function browsersync () {
+  browserSync.init({
+    server: {
+      baseDir: "app/"
+    }
+  });
+}
+
+function watching() {
+  watch(['app/sass/style.sass'], styles)
+  watch(['app/js/main.js'], scripts)
+  watch(['app/**/*.html']).on('change', browserSync.reload);
+}
+
+function scripts() {
+  return src('app/js/main.js')
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
+}
+
+
+function styles () {
+  return src('app/sass/style.sass')
+  .pipe(concat('style.min.css'))
+  .pipe(sass({outputStyle: 'compressed' }))
+  .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
+}
+
+
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.default = parallel(styles, scripts, browsersync, watching);
