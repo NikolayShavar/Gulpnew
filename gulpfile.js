7cb2de9f73cde3211d744b551976f60afd9f9b89
NikolayShavar <fargon45821@gmail.com>
2024-02-02 11:59:43 +0200

    0202

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
-const {src, dest, watch, parallel} = require('gulp'); //основной плагин
-const sass = require('gulp-sass')(require('sass')); //плагин для стилей
-const concat = require('gulp-concat')               //плагин для переименований и минификаций файлов, еще он вроде объединять умеет. 
-const uglify = require('gulp-uglify-es').default;  // плагин для js 
-const browserSync = require('browser-sync').create();// живой сервер, показывает изменения в браузере 
-const rename = require('gulp-rename')// переименовыватель 
+const {src, dest, watch, parallel, serias} = require('gulp');      //основной плагин, serias щтвечает за последовательность. 
+const sass = require('gulp-sass')(require('sass'));        //плагин для стилей
+const concat = require('gulp-concat')                      //плагин для переименований и минификаций файлов, еще он вроде объединять умеет. 
+const uglify = require('gulp-uglify-es').default;          // плагин для js 
+const browserSync = require('browser-sync').create();      // живой сервер, показывает изменения в браузере 
+const rename = require('gulp-rename');                     // переименовыватель 
+const autoprefixer = require('gulp-autoprefixer');         // автопрефиксер для древних браузеров
+const clean = require('gulp-clean');                       //удаляет файлы
 
 
-
-function browsersync () {
+function browsersync () {       //Живой сервер
   browserSync.init({
     server: {
       baseDir: "app/"
@@ -15,13 +16,13 @@ function browsersync () {
   });
 }
 
-function watching() {
-  watch(['app/sass/style.sass'], styles)
-  watch(['app/js/main.js'], scripts)
-  watch(['app/**/*.html']).on('change', browserSync.reload);
+function watching() {            //СЛЕДИЛКА
+  watch(['src/sass/style.sass'], styles)
+  watch(['src/js/main.js'], scripts)
+  watch(['src/**/*.html']).on('change', browserSync.reload);
 }
 
-function scripts() {
+function scripts() {            //обработчик js
   return src('src/js/main.js')
     .pipe(concat('main.min.js'))
     .pipe(uglify())
@@ -30,22 +31,43 @@ function scripts() {
 }
 
 
-function styles () {
-  return src('app/sass/style.sass')
+function styles () {            //обработчик Стилей
+  return src('src/sass/style.sass')
   .pipe(sass())
+  .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version']}))
   .pipe(dest('src/css'))
   .pipe(sass({outputStyle: 'compressed' }))
   .pipe(rename('style.min.css'))
   .pipe(dest('app/css'))
-    .pipe(browserSync.stream())
+  .pipe(browserSync.stream())
 }
 
 
 
+function html () {            //перекидывает все html из src в app 
+  return src('src/**/*.html')
+  .pipe(dest('app/'))
+}
+
+function cleanapp() {
+  return src('app')
+  .pipe(clean())
+}
+
+// function build() {
+//   return src ([
+//     'app/css/style.min.css',
+//     'app/js/main.min.js',
+//     'app/**/*.html'
+//   ], {base : 'app'})
+//   .pipe(dest('dist'))
+// }
+
+exports.html = html;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watching = watching;
 exports.browsersync = browsersync;
+exports.cleanapp = cleanapp;
 
-
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(html ,styles, scripts, browsersync, watching);
